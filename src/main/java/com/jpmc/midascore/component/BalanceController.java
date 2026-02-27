1a5a513e86072b8c30c171401cf680e390911bc4
Aryansharma906 <76518533+Aryansharma906@users.noreply.github.com>
2026-02-27 16:12:12 +0530

    Refactor BalanceController to use UserRecordRepository

--- src/main/java/com/jpmc/midascore/component/BalanceController.java
+++ src/main/java/com/jpmc/midascore/component/BalanceController.java
@@ -2,26 +2,25 @@ package com.jpmc.midascore.component;
 
 import com.jpmc.midascore.entity.UserRecord;
 import com.jpmc.midascore.foundation.Balance;
-import com.jpmc.midascore.repository.UserRepository;
+import com.jpmc.midascore.repository.UserRecordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
 
 @RestController
 public class BalanceController {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserRecordRepository UserRecordRepository;
 
     @GetMapping("/balance")
     public Balance getBalance(@RequestParam("userId") Long userId) {
-        Optional<UserRecord> userRecord = userRepository.findById(userId);
+        UserRecord userRecord = UserRecordRepository.findById(userId);
         
-        if (userRecord.isPresent()) {
-            return new Balance(userRecord.get().getBalance());
+        if (userRecord != null) {
+            return new Balance(userRecord.getBalance());
         } else {
             return new Balance(0.0f);
         }
